deb2a9ba2f6be8d1dba0f69779c5089fd1101a09
git4cc-noob <ktcc.0822@gmail.com>
2023-01-19 11:11:27 +0100

    Einzelparameter

--- script_01.js
+++ script_01.js
@@ -7,15 +7,25 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-
-
-
-
+// output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) 
+{
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP + 
+                word2 + GAP + 
+                word3 + GAP + 
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
+                PUNCT;
+    return str;
+    
+}
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
